8ac6f426d5ab2bff789e86c40899a90fcafcb76c
Joseph Maramba <josephmaramba50@gmail.com>
2022-03-27 13:45:56 +0300

    model setup

--- server/models/properties.js
+++ server/models/properties.js
@@ -1,11 +1,14 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const propertiesSchema = mongoose.Schema({
-//   name: String,
-//   description: String,
-//   tags: [String],
-// });
+const propertiesSchema = mongoose.Schema(
+  {
+    name: { type: String, required: [true, "Kindly fill before submitting"] },
+    description: String,
+    tags: [String],
+  },
+  { timestamps: true }
+);
 
-// const propertiesModel = mongoose.model("propertiesModel", propertiesSchema);
+const propertiesModel = mongoose.model("propertiesModel", propertiesSchema);
 
-// export default propertiesModel;
+export default propertiesModel;
