965e688ecf6f622f26798fc05d835a648ba73704
Joseph Maramba <josephmaramba50@gmail.com>
2022-03-31 11:21:06 +0300

    changes to package.json

--- backend/app.js
+++ backend/app.js
@@ -14,9 +14,9 @@ const PORT = process.env.PORT || 3000;
 dbConnect();
 
 // middleware
+app.use(cors());
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ extended: false, limit: "30mb" }));
-app.use(cors());
 app.use(morgan("dev"));
 
 //routes
@@ -25,11 +25,6 @@ app.use("/api/quotes", quoteRoutes);
 //test if API works
 app.get("/", (req, res) => {
   res.status(200).json({ message: "API works" });
-});
-
-//test app on deploy
-app.get("/", (req, res) => {
-  res.send("API works!");
 });
 
 // server connection
